73681d795ae5dabda919be8cfd3527a4f5969e0c
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-06-01 11:54:28 -0500

    playing with the dom

---
+++ dom/quick_dom/index.js
@@ -0,0 +1,6 @@
+let arr = document.getElementsByTagName("li").length;
+let arr2 = document.getElementsByClassName("btn");
+let arr3 = document.getElementById("title");
+let qs = document.querySelector('h1');
+let qa = document.querySelectorAll('#list .item');
+document.querySelector('li a').style.color = "red";
