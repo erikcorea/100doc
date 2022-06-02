971547be571a11fa58cf2097e1fd71086c8638b8
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-06-02 10:55:32 -0500

    dom review

--- dom/quick_dom/index.js
+++ dom/quick_dom/index.js
@@ -4,3 +4,15 @@ let arr3 = document.getElementById("title");
 let qs = document.querySelector('h1');
 let qa = document.querySelectorAll('#list .item');
 document.querySelector('li a').style.color = "red";
+document.querySelector('button').style.backgroundColor = "yellow"
+document.querySelector('button').classList.add('invisible');
+document.querySelector('button').classList.remove('invisible');
+document.querySelector('button').classList.toggle('invisible');
+
+document.querySelector("h1").classList.add('huge');
+
+document.getElementById('title').textContent;
+
+document.querySelector('a').attributes;
+document.querySelector('a').getAttribute('href');
+document.querySelector('a').setAttribute('href', "https://www.bing.com");
