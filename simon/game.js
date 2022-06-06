6881f539ad8f5b1838718062ce356baacf79f271
erikcorrea444@gmail.com <erikcorrea444@gmail.com>
2022-06-06 11:56:57 -0500

    adding sound to clicking button?

--- simon/game.js
+++ simon/game.js
@@ -1,5 +1,7 @@
 let buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
+[...document.querySelectorAll('.btn')].map((button) => button.addEventListener('click', handleClick));
 
 
 function nextSequence(){
@@ -13,6 +15,21 @@ function nextSequence(){
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
     let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play()
+}
+
+
+function handleClick(){
+    let buttonId = this.id;
+    let userChosenColor = document.getElementById(buttonId);
+    userClickedPattern.push(userChosenColor);
+    console.log(userChosenColor);
+
+    playSound(userChosenColor);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play()
 }
 
